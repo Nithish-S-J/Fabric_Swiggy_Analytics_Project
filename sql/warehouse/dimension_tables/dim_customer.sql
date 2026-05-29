046d092cf0635dabf0ef09ab387ed07c93916353
Nithish_S_J <96019176+Nithish-S-J@users.noreply.github.com>
2026-05-29 10:32:05 +0530

    Update dim_customer.sql

--- sql/warehouse/dimension_tables/dim_customer.sql
+++ sql/warehouse/dimension_tables/dim_customer.sql
@@ -1,25 +1,35 @@
 DROP TABLE IF EXISTS dbo.dim_customers;
 GO
 
-CREATE TABLE dbo.dim_customers (
-    customer_id VARCHAR(100) NOT NULL,
-    customer_segment VARCHAR(50),
-    customer_type VARCHAR(50)
+CREATE TABLE dbo.dim_customers
+(
+    customer_id VARCHAR(50),
+    total_orders INT,
+    avg_order_value DECIMAL(10,2),
+    avg_service_rating DECIMAL(10,2),
+    customer_segment VARCHAR(50)
 );
 GO
 
 INSERT INTO dbo.dim_customers
-SELECT 
+
+SELECT
+
     customer_id,
-    CASE 
-        WHEN SUM(order_value_inr) > 12000 THEN 'Platinum VIP'
-        WHEN COUNT(order_id) > 10 THEN 'High Frequency'
-        ELSE 'Standard'
-    END AS customer_segment,
-    CASE 
-        WHEN COUNT(order_id) = 1 THEN 'New Customer'
-        ELSE 'Repeat Customer'
-    END AS customer_type
+
+    COUNT(order_id) AS total_orders,
+
+    AVG(order_value_inr) AS avg_order_value,
+
+    AVG(service_rating) AS avg_service_rating,
+
+    CASE
+        WHEN COUNT(order_id) >= 20 THEN 'High Value'
+        WHEN COUNT(order_id) >= 10 THEN 'Medium Value'
+        ELSE 'Low Value'
+    END AS customer_segment
+
 FROM dbo.silver_orders
+
 GROUP BY customer_id;
 GO
